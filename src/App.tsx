9cd9a819debb40220be0046d2a04af7f00dd94b5
Daniel <Goeloe.daniel@gmail.com>
2025-03-12 12:42:57 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-600 text-2xl font-semibold">
+        This is a test
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Includi anche i file .tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
